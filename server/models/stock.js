f11fb061c8672e426cf7ae3174530ca1f60f61b2
suhail_yousaf <ysuhail444@gmail.com>
2023-10-07 13:14:27 +0530

    commit

--- server/models/stock.js
+++ server/models/stock.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-const stockSchema = mongoose.Schema({
+const { Schema } = mongoose;
+const stockSchema = new Schema({
     name:String,
     Price:Number,
 },
