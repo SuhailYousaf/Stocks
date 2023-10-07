d0749fb70ac0aa4a7e0ef6c5710dd71112bd5093
suhail_yousaf <ysuhail444@gmail.com>
2023-10-07 15:36:09 +0530

    commit

--- client/src/Api/Api.js
+++ client/src/Api/Api.js
@@ -1,9 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
+
+// Create an Axios instance with a base URL
 const API = axios.create({ baseURL: 'https://stocks-pjca.onrender.com' });
 
+// Export functions for making specific API requests
 
+// Create a new stock
 export const createStock = (data) => API.post('/create', data);
 
-export const getStocks = () => API.get(`/getStocks`);
+// Get a list of stocks
+export const getStocks = () => API.get('/getStocks');
 
+// Update a stock by its ID
 export const Update = (id, updatedData) => API.put(`/update/${id}`, updatedData);
