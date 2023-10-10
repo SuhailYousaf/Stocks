fe4c096fad06c2a230c4c7958c31d5ed92f70e59
suhail_yousaf <ysuhail444@gmail.com>
2023-10-11 01:18:37 +0530

    some updattion

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import './Home.css'; 
+import './Home.css';
 import { createStock, getStocks, Update } from '../Api/Api';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
   const [stocks, setStocks] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [newStockName, setNewStockName] = useState('');
   const [newStockPrice, setNewStockPrice] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [stocksPerPage] = useState(5); // Number of stocks to display per page
 
   useEffect(() => {
     const fetchStocks = async () => {
@@ -14,8 +18,8 @@ const Home = () => {
         const response = await getStocks();
 
         if (response.status === 200) {
-          const responseData = response.data; // Assuming the data object has the structure { Stocks: [] }
-          const stocksArray = responseData.Stocks || []; // Extract the Stocks array
+          const responseData = response.data;
+          const stocksArray = responseData.Stocks || [];
 
           setStocks(stocksArray);
         }
@@ -28,14 +32,12 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    // Function to update stock prices randomly
     const updateStockPricesRandomly = () => {
       const updatedStocks = stocks.map(async (stock) => {
-        const randomPriceChange = (Math.random() - 0.5) * 10; // Generate a random price change
+        const randomPriceChange = (Math.random() - 0.5) * 10;
         const newPrice = stock.Price + randomPriceChange;
 
         try {
-          // Make an API call to update the stock price
           const response = await Update(stock._id, { Price: newPrice });
 
           if (response.status === 200) {
@@ -55,16 +57,19 @@ const Home = () => {
       });
     };
 
-    // Update stock prices randomly every 3 seconds
     const priceUpdateInterval = setInterval(updateStockPricesRandomly, 3000);
 
     return () => {
-      clearInterval(priceUpdateInterval); // Cleanup the interval on component unmount
+      clearInterval(priceUpdateInterval);
     };
   }, [stocks]);
 
   const handleNewStockClick = () => {
     setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
   };
 
   const handleAddNewStock = async () => {
@@ -76,23 +81,44 @@ const Home = () => {
     try {
       const newStock = {
         name: newStockName,
-        Price: parseFloat(newStockPrice), // Use the correct property name from your API response
+        Price: parseFloat(newStockPrice),
       };
 
       const response = await createStock(newStock);
 
-      if (response.status === 200) {
-        const createdStock = response.data;
-        setStocks([...stocks, createdStock]);
+      if (response.status === 201) {
         setNewStockName('');
         setNewStockPrice('');
-        setIsModalOpen(false);
+
+        toast.success('New stock has been added successfully!', {
+          position: 'top-right',
+          autoClose: 3000,
+        });
+
+        setTimeout(() => {
+          window.location.reload();
+        }, 3000);
       } else {
         console.log('Failed to create a new stock');
+
+        toast.error('Failed to create a new stock', {
+          position: 'top-right',
+          autoClose: 3000,
+        });
       }
     } catch (error) {
       console.error('Error creating a new stock', error);
     }
+  };
+
+  // Calculate the range of stocks to display on the current page
+  const indexOfLastStock = currentPage * stocksPerPage;
+  const indexOfFirstStock = indexOfLastStock - stocksPerPage;
+  const currentStocks = stocks.slice(indexOfFirstStock, indexOfLastStock);
+
+  // Function to change the current page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
 
   return (
@@ -113,7 +139,7 @@ const Home = () => {
           </tr>
         </thead>
         <tbody>
-          {stocks.map((stock) => (
+          {currentStocks.map((stock) => (
             <tr key={stock._id}>
               <td>{stock._id}</td>
               <td>{stock.name}</td>
@@ -122,30 +148,45 @@ const Home = () => {
           ))}
         </tbody>
       </table>
-
+      <div className="pagination">
+        {Array.from({ length: Math.ceil(stocks.length / stocksPerPage) }).map(
+          (_, index) => (
+            <button
+              key={index}
+              onClick={() => paginate(index + 1)}
+              className={currentPage === index + 1 ? 'active' : ''}
+            >
+              {index + 1}
+            </button>
+          )
+        )}
+      </div>
       {isModalOpen && (
-        <div className="modal">
-          <div className="modal-content">
+        <div className="modal" onClick={handleCloseModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
             <h2>Add New Stock</h2>
-            <label htmlFor="newStockName">Name:</label>
             <input
               type="text"
               id="newStockName"
+              placeholder="Name"
               value={newStockName}
               onChange={(e) => setNewStockName(e.target.value)}
             />
-            <label htmlFor="newStockPrice">Price:</label>
             <input
               type="text"
               id="newStockPrice"
+              placeholder="Price"
               value={newStockPrice}
               onChange={(e) => setNewStockPrice(e.target.value)}
             />
-            <button onClick={handleAddNewStock}>Add</button>
-            <button onClick={() => setIsModalOpen(false)}>Cancel</button>
+            <div>
+              <button onClick={handleAddNewStock}>Add</button>
+              <button onClick={handleCloseModal}>Cancel</button>
+            </div>
           </div>
         </div>
       )}
+      <ToastContainer position="top-right" autoClose={3000} />
     </div>
   );
 };
